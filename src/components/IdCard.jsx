28ea2636c62c75e2d5530dfba0a695c320484e3e
Gerard Solanes <gerardsolher@gmail.com>
2023-02-27 11:54:17 +0100

    Reformats date object

--- src/components/IdCard.jsx
+++ src/components/IdCard.jsx
@@ -1,16 +1,21 @@
 import React from "react";
 
 function IdCard({ firstName, lastName, gender, height, birth, picture }) {
-    const fromatedDate = birth.toISOString().substring(0, 10);
+    const fromatedDate = birth.toLocaleDateString('en-US', {
+        weekday: 'short',
+        month: 'short',
+        day: 'numeric',
+        year: 'numeric'
+    });
     return ( 
         <div className="card">
             <img src={picture} alt="Profile" />
             <div className="card-content">
-                <p><span className="title">First name:</span>{firstName}</p>
-                <p><span className="title">Last name:</span>{lastName}</p>
-                <p><span className="title">Gender:</span>{gender}</p>
-                <p><span className="title">Height:</span>{height}</p>
-                <p><span className="title">Birth:</span>{fromatedDate}</p>
+                <p><span className="title">First name: </span>{firstName}</p>
+                <p><span className="title">Last name: </span>{lastName}</p>
+                <p><span className="title">Gender: </span>{gender}</p>
+                <p><span className="title">Height: </span>{height}</p>
+                <p><span className="title">Birth: </span>{fromatedDate}</p>
             </div>
         </div>
      );
