c3174e64da23e8d394478cf363e45fcf78a435e3
Gerard Solanes <gerardsolher@gmail.com>
2023-02-27 13:40:31 +0100

    Complete iteration 7

--- src/App.js
+++ src/App.js
@@ -3,8 +3,9 @@ import './App.css';
 // import Greetings from './components/Greetings';
 // import Random from './components/Random';
 // import BoxColor from './components/BoxColor';
-import CreditCard from './components/CreditCard';
-import Rating from './components/Rating';
+// import CreditCard from './components/CreditCard';
+// import Rating from './components/Rating';
+import DriverCard from './components/DriverCard';
 
 function App() {
   return (
@@ -76,12 +77,34 @@ function App() {
         color="white" 
       /> */}
 
+
+      {/* Iteration 6
       <Rating>0</Rating>
       <Rating>1.49</Rating>
       <Rating>1.5</Rating>
       <Rating>3</Rating>
       <Rating>4</Rating>
-      <Rating>5</Rating>
+      <Rating>5</Rating> */}
+
+      <DriverCard
+        name="Travis Kalanick"
+        rating={4.2}
+        img="https://si.wsj.net/public/resources/images/BN-TY647_37gql_OR_20170621052140.jpg?width=620&height=428"
+        car={{
+          model: "Toyota Corolla Altis",
+          licensePlate: "CO42DE"
+        }}
+      />
+
+      <DriverCard
+        name="Dara Khosrowshahi"
+        rating={4.9}
+        img="https://ubernewsroomapi.10upcdn.com/wp-content/uploads/2017/09/Dara_ELT_Newsroom_1000px.jpg"
+        car={{
+          model: "Audi A3",
+          licensePlate: "BE33ER"
+        }}
+      />
     </div>
   );
 }
