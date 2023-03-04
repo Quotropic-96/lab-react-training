9f0df0391b9a4f0c8cbbcca0f4234c04e7cbfe71
Gerard Solanes <gerardsolher@gmail.com>
2023-03-04 13:39:40 +0100

    WIP. Carousel missing handle right

---
+++ src/components/Carousel.jsx
@@ -0,0 +1,25 @@
+import { useEffect, useState } from "react";
+
+function Carousel({images}) {
+    const [imgIdx, setImgIdx] = useState(0)
+    const [image, setImage] = useState(images[imgIdx]);
+
+    useEffect(() => {
+        setImage(images[imgIdx])
+    }, [imgIdx])
+
+    const handleLeft = () => {
+        if (imgIdx > 0) {
+            setImgIdx(prev => prev - 1);
+        }
+    }
+    return ( 
+        <div>
+            <img src={image} alt="Default" />
+            <button onClick={handleLeft}>Left</button>
+            <button>Right</button>
+        </div>
+     );
+}
+
+export default Carousel;
