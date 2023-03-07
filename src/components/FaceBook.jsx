e74f702e81695ca5985feffbcb4f6d4d5f3027ee
Gerard Solanes <gerardsolher@gmail.com>
2023-03-07 09:34:35 +0100

    Complete iteration 14

--- src/components/FaceBook.jsx
+++ src/components/FaceBook.jsx
@@ -1,10 +1,28 @@
+import { useState } from 'react';
 import profiles from '../data/berlin.json';
 
 function FaceBook() {
+    const auxCountries = [];
+    const allCountries = profiles.map(elem => elem.country).filter(elem => {
+        if (!auxCountries.includes(elem)) {
+            auxCountries.push(elem);
+            return true;
+        }
+        return false;
+    });
+    const [selectCountry, setSelectCountry] = useState('All');
+
+    const handleSelectCountry = (e) => {
+        setSelectCountry(e.target.value);
+    }
     return ( 
         <div className="faceBook">
+            <div className="countryBar">
+                <button key={'All'} className={'All' === selectCountry ? 'selectedCountry': ''} value={'All'} onClick={handleSelectCountry}>All</button>
+                {allCountries.map(elem => <button key={elem} value={elem} className={elem === selectCountry ? 'selectedCountry': ''} onClick={handleSelectCountry}>{elem}</button>)}
+            </div>
             {profiles.map((elem, idx) => {
-                return <div key={idx} className='faceBookCard'>
+                return <div key={idx} className={elem.country === selectCountry ? 'faceBookCard selectedCountry' : 'faceBookCard'}>
                     <img src={elem.img} alt={elem.firstName} />
                     <div className="faceBookInfo">
                         <p><span>First name:</span> {elem.firstName}</p>
